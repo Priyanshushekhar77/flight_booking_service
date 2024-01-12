a5bcab033fe5d536461bed795394020d306f7f3d
Priyanshushekhar77 <way6shekhar@gmail.com>
2024-01-12 12:34:11 +0530

    gateway task

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const setupAndStartServer = async()=>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    app.use("/api", apiRoutes);
+    app.use("/bookingservice/api", apiRoutes);// from api_gateway
 
     app.listen(PORT, async()=>{
         console.log(`Server Running at PORT : ${PORT}`);
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 
 const bookingController = new BookingController();
-
+router.get('/info',(req,res) => {
+    return res.json({message:'Responds from booking route through api gateway'})
+})
 router.post("/booking", bookingController.createBooking);
 router.post('/publish',bookingController.SendMessageToQueue);
 
